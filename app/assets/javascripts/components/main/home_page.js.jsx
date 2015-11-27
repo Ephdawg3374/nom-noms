c259be5077b4071072d7af11397ea5a75f91fc01
Ephraim Pei <ephdawg3374@gmail.com>
2015-11-26 23:25:26 -0500

    splash page complete. plays video and fades in messages.

--- app/assets/javascripts/components/main/home_page.js.jsx
+++ app/assets/javascripts/components/main/home_page.js.jsx
@@ -1,8 +1,51 @@
 var HomePage = React.createClass({
+  getInitialState: function () {
+    return (
+      {
+        message1: false,
+        message2: false,
+        message3: false
+      }
+     );
+  },
+
+  componentDidMount: function () {
+    setTimeout(function () {
+      this.setState({ message1: true });
+    }.bind(this), 2000);
+
+    setTimeout(function () {
+      this.setState({ message2: true });
+    }.bind(this), 5000);
+
+    setTimeout(function () {
+      this.setState({ message3: true });
+    }.bind(this), 8000);
+  },
+
   render: function () {
+    var msg1Text = "Tryin' to Nom?";
+    var msg2Text = "Use the search bars. Go nom or go home.";
+    var msg3Text = "Unless you are already home...then stay there...";
+    var msgBaseClass = "home-page-splash-msg ";
+
+    var msg1Class = msgBaseClass + "first " + (this.state.message1 ? "active" : "inactive");
+    var msg2Class = msgBaseClass + "second " + (this.state.message2 ? "active" : "inactive");
+    var msg3Class = msgBaseClass + "third " + (this.state.message3 ? "active" : "inactive");
+
     return (
       <div className="home-page">
-        
+        <video
+          className="home-page-splash-video"
+          src="https://s3.amazonaws.com/nom-noms-dev/videos/avengers_shawarma_scene_no_sound.mp4"
+          autoPlay
+          loop
+          type="video/mp4">
+        </video>
+
+        <label className={msg1Class}>{ msg1Text }</label>
+        <label className={msg2Class}>{ msg2Text }</label>
+        <label className={msg3Class}>{ msg3Text }</label>
       </div>
     );
   }
