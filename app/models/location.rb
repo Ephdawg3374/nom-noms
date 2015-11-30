9756b600d6bc2a7d4c0a83886ddb356401e5a174
Ephraim Pei <ephdawg3374@gmail.com>
2015-11-29 19:29:03 -0500

    fixed name search. now correctly searches for location names/cuisines/types by respecting partial matches.

--- app/models/location.rb
+++ app/models/location.rb
@@ -11,12 +11,9 @@ class Location < ActiveRecord::Base
     location_type = search_params[:searchType]
     price_range = search_params[:priceRange]
 
-    search_params[:priceRange] == "All" ? price_range = "%" : price_range += "%"
-    # price_range = "%" if search_params[:priceRange] == "All"
+    price_range = "%" if search_params[:priceRange] == "All"
     # pull all location types if no type specified
-    location_type.empty? ? location_type= "%" : location_type += "%"
-
-    # location_type = "%" if location_type.empty?
+    location_type.empty? ? location_type = "%" : location_type += "%"
 
     search_location_coords = Location.get_search_loc_coords(area)
 
@@ -24,70 +21,39 @@ class Location < ActiveRecord::Base
       lat: search_location_coords[0],
       lng: search_location_coords[1],
       distance: distance,
-      location_type: parse_location(location_type)[0],
+      location_type: parse_location(location_type).join("%"),
       price_range: price_range
     }
 
-    exact_loc_match = Location.where("locations.name = ?", location_type)
-
-    # if loc type search matches an exact name location, search for locations with that
-    # name and ignore price range filter (but respect distacne range filter)
-    if exact_loc_match.empty?
-      Location.find_by_sql [
-        "SELECT
-          *
-        FROM
-          (
-            SELECT
-              locations.*,
-               asin(
-                sqrt(
-                  sin(radians(:lat-lat)/2)^2 +
-                  sin(radians(:lng-lng)/2)^2 *
-                  cos(radians(:lat)) *
-                  cos(radians(:lng))
-                  )
-                ) * 7926.3352 AS distance
-            FROM
-              locations
-          ) AS loc_w_distance
-        WHERE
-          loc_w_distance.distance < :distance AND
-          (location_type LIKE :location_type OR
-          cuisine LIKE :location_type OR
-          name LIKE :location_type) AND
-          price_range LIKE :price_range
-        ORDER BY
-          loc_w_distance.distance
-        LIMIT
-          100",
-        sql_query_params]
-    else
-      Location.find_by_sql [
-        "SELECT
-          *
-        FROM
-          (
-            SELECT
-              locations.*,
-               asin(
-                sqrt(
-                  sin(radians(:lat-lat)/2)^2 +
-                  sin(radians(:lng-lng)/2)^2 *
-                  cos(radians(:lat)) *
-                  cos(radians(:lng))
-                  )
-                ) * 7926.3352 AS distance
-            FROM
-              locations
-          ) AS loc_w_distance
-        WHERE
-          loc_w_distance.distance < :distance AND
-          name = :location_type
-        ORDER BY
-          loc_w_distance.distance",
-        sql_query_params]
-      end
+    Location.find_by_sql [
+      "SELECT
+        *
+      FROM
+        (
+          SELECT
+            locations.*,
+             asin(
+              sqrt(
+                sin(radians(:lat-lat)/2)^2 +
+                sin(radians(:lng-lng)/2)^2 *
+                cos(radians(:lat)) *
+                cos(radians(:lng))
+                )
+              ) * 7926.3352 AS distance
+          FROM
+            locations
+        ) AS loc_w_distance
+      WHERE
+        loc_w_distance.distance < :distance AND
+        (location_type LIKE :location_type OR
+        cuisine LIKE :location_type OR
+        name LIKE :location_type) AND
+        price_range LIKE :price_range
+      ORDER BY
+        loc_w_distance.distance
+      LIMIT
+        100",
+      sql_query_params]
   end
 
   def self.get_search_loc_coords(search_location)
@@ -152,7 +118,7 @@ class Location < ActiveRecord::Base
       return []
     end
 
-    loc_type_partial = loc_type_partial.capitalize + "%"
+    loc_type_partial = parse_location(loc_type_partial)[0] + "%"
 
     types = Location.select(:location_type)
       .where("location_type LIKE ?", loc_type_partial)
@@ -162,20 +128,24 @@ class Location < ActiveRecord::Base
       .where("cuisine LIKE ?", loc_type_partial)
       .distinct.limit(3)
 
-    names = Location.find_by_sql(
+    names = Location.find_by_sql([
       "SELECT
         locations.name
       FROM
         locations
-      JOIN
+      LEFT OUTER JOIN
         reviews
       ON
         reviews.location_id = locations.id
+      WHERE
+        locations.name LIKE ?
       GROUP BY
-        locations.id
+        locations.name
       ORDER BY
-        avg(reviews.rating) DESC"
-      )
+        avg(reviews.rating) DESC
+      LIMIT
+        3",
+      loc_type_partial])
     # names = Location.select(:name, :reviews)
     #   .where("name LIKE ?", loc_type_partial).order().limit(3)
     (types + cuisines + names)
