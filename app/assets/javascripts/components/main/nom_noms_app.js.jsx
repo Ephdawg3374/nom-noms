cd479e8f4666084d38467fc0cadf960e0eea3dc8
ephraimpei <ephraimpei@gmail.com>
2015-12-05 21:10:15 -0500

    error added to NomNoms App component

--- app/assets/javascripts/components/main/nom_noms_app.js.jsx
+++ app/assets/javascripts/components/main/nom_noms_app.js.jsx
@@ -1,4 +1,22 @@
 var NomNomsApp = React.createClass({
+  getInitialState: function () {
+    return (
+      {
+        isValid: true,
+        errors: []
+      }
+    );
+  },
+
+  showErrors: function (errors) {
+    this.setState(
+      {
+        isValid: false,
+        errors: errors
+      }
+    );
+  },
+
   componentWillMount: function () {
     window.NomNomsApp.searchAuto = false;
 
@@ -15,8 +33,28 @@ var NomNomsApp = React.createClass({
   },
 
   render: function () {
+    var errors;
+    var errorClass = "errors ";
+
+    if (!this.state.isValid) {
+      errors = this.state.errors.map( function(err, idx) {
+        return <li key={idx}>{err}</li>;
+      });
+
+      errorClass += "display";
+
+      window.setTimeout(function () {
+        $(".errors").removeClass("display");
+      }, 0);
+    }
+
     return (
       <div className="main-app">
+
+        <div className={ errorClass }>
+          { errors }
+        </div>
+
         <header className="header">
           <NavigationBar />
         </header>
